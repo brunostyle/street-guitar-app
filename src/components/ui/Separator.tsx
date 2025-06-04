2265751544bb62e1227e894b11f9c8a0841485f7
brunobrianmillalipe <brunobrianmillalipe@gmail.com>
2025-06-04 03:59:19 +0000

    separator color fixed

--- src/components/ui/Separator.tsx
+++ src/components/ui/Separator.tsx
@@ -4,8 +4,8 @@ interface IProps {
 
 export const Separator = ({ children }: IProps) => (
     <div className="flex justify-center items-center">
-        <div className="w-full h-[1px] bg-gradient-to-r from-transparent via-gray-800 to-gray-600" />
+        <div className="w-full h-[1px] bg-gradient-to-r from-transparent via-default-100 to-default-300" />
         <span className="px-3 text-gray-500 text-sm">{children}</span>
-        <div className="w-full h-[1px] bg-gradient-to-l from-transparent via-gray-800 to-gray-600" />
+        <div className="w-full h-[1px] bg-gradient-to-l from-transparent via-default-100 to-default-300" />
     </div>
 )
