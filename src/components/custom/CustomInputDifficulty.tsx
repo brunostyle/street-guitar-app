0ccd124de4109e4da313f4dbad224e8ea923d473
brunobrianmillalipe <brunobrianmillalipe@gmail.com>
2025-06-18 20:08:17 +0000

    buttons start uptimised

--- src/components/custom/CustomInputDifficulty.tsx
+++ src/components/custom/CustomInputDifficulty.tsx
@@ -3,18 +3,19 @@ import { CustomButtonIcon } from "@components";
 import { IoStarOutline, IoStarSharp } from "react-icons/io5";
 import { useField } from "formik";
 
+const stars = [1, 2, 3, 4, 5];
+
 export const CustomInputDifficulty = () => {
     const [field, _meta, helpers] = useField('difficulty');
-    const stars = [1, 2, 3, 4, 5];
-
     return (
         <div>
             <h4 className="text-xs mb-2">Dificultad</h4>
             <Flex space="gap-0">
-                {stars.map(star => star <= field.value
-                    ? <CustomButtonIcon key={star} variant="light" color="primary" onPress={() => helpers.setValue(star)}><IoStarSharp size="1.2em" /></CustomButtonIcon>
-                    : <CustomButtonIcon key={star} variant="light" color="primary" onPress={() => helpers.setValue(star)}><IoStarOutline size="1.2em" opacity=".5" /></CustomButtonIcon>
-                )}
+                {stars.map(star => (
+                    <CustomButtonIcon key={star} variant="light" color="primary" onPress={() => helpers.setValue(star)}>
+                        {star <= field.value ? <IoStarSharp size="1.2em" /> : <IoStarOutline size="1.2em" opacity=".5" />}
+                    </CustomButtonIcon>
+                ))}
             </Flex>
         </div>
     )
--- src/components/ui/Difficulty.tsx
+++ src/components/ui/Difficulty.tsx
@@ -6,15 +6,14 @@ interface IProps {
     difficulty: number;
 }
 
-export const Difficulty = ({ difficulty }: IProps) => {
-    const stars = [1, 2, 3, 4, 5];
+const stars = [1, 2, 3, 4, 5];
 
-    return (
-        <Flex space="gap-0">
-            {stars.map(star => star <= difficulty
-                ? <CustomButtonIcon key={star} variant="light" color="primary"><IoStarSharp size="1.2em" /></CustomButtonIcon>
-                : <CustomButtonIcon key={star} variant="light" color="primary"><IoStarOutline size="1.2em" opacity=".5" /></CustomButtonIcon>
-            )}
-        </Flex>
-    )
-}
+export const Difficulty = ({ difficulty }: IProps) => (
+    <Flex space="gap-0">
+        {stars.map(star => (
+            <CustomButtonIcon key={star} variant="light" color="primary">
+                {star <= difficulty ? <IoStarSharp size="1.2em" /> : <IoStarOutline size="1.2em" opacity=".5" />}
+            </CustomButtonIcon>
+        ))}
+    </Flex>
+)
