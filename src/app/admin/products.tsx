eb12a8ad35bebd93977f2c26a11a29d4a58f7073
brunostyle <brunomstyle@gmail.com>
2025-07-21 02:16:47 +0000

    feat(admin products page): difficulty added

--- src/app/admin/products.tsx
+++ src/app/admin/products.tsx
@@ -1,7 +1,7 @@
 import { Avatar, Pagination, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@heroui/react";
 import { useNavigate } from "react-router-dom";
 import { IoPencil, IoEyeOutline, IoImageOutline, IoPricetagsOutline } from "@icons";
-import { FullScreenLoading, Admin, Nothing, CustomButtonIcon, CustomButtonLink } from "@components";
+import { FullScreenLoading, Admin, Nothing, CustomButtonIcon, CustomButtonLink, Difficulty } from "@components";
 import { HiddenTitle, HiddenSubtitle, ChipCategory } from "@styles";
 import { usePaginateProducts } from "@hooks";
 import { ROUTES } from "@navigation";
@@ -23,6 +23,7 @@ const Products = () => {
                      <TableColumn>TITULO</TableColumn>
                      <TableColumn>DESCRIPCIÓN</TableColumn>
                      <TableColumn>PRECIO</TableColumn>
+                     <TableColumn>DIFICULTAD</TableColumn>
                      <TableColumn>CATEGORIA</TableColumn>
                      <TableColumn>TAB</TableColumn>
                      <TableColumn>EDITAR</TableColumn>
@@ -34,6 +35,7 @@ const Products = () => {
                            <TableCell><HiddenTitle>{product.title}</HiddenTitle></TableCell>
                            <TableCell><HiddenSubtitle>{product.description}</HiddenSubtitle></TableCell>
                            <TableCell><h4>${product.price}</h4></TableCell>
+                           <TableCell><Difficulty difficulty={product.difficulty} /></TableCell>
                            <TableCell><ChipCategory>{product.category}</ChipCategory></TableCell>
                            <TableCell><CustomButtonLink to={product.pdf!} isDisabled={!product.pdf}><IoEyeOutline /></CustomButtonLink></TableCell>
                            <TableCell><CustomButtonIcon onPress={() => router(ROUTES.updateProduct + product.id)}><IoPencil /></CustomButtonIcon></TableCell>
