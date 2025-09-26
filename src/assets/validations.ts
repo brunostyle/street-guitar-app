fd9e8ceb781f9edae33197f5552c9c6ed139d470
brunostyle <brunomstyle@gmail.com>
2025-09-26 02:59:31 -0300

    fix: yup imports simplified

--- src/assets/validations.ts
+++ src/assets/validations.ts
@@ -1,60 +1,41 @@
 import { ValidCategory } from '@interfaces';
-import * as Yup from 'yup'
+import { array, date, mixed, number, object, string } from 'yup'
 
-export const searchSchema = Yup.object({
-   query: Yup.string().trim().required()
+export const searchSchema = object({
+   query: string().trim().required()
 })
 
-export const loginSchema = Yup.object({
-   email: Yup.string().trim().required('Campo requerido').email('Correo electronico no valido'),
-   password: Yup.string().trim().required('Campo requerido')
+export const loginSchema = object({
+   email: string().trim().required('Campo requerido').email('Correo electronico no valido'),
+   password: string().trim().required('Campo requerido')
 })
 
-export const registerSchema = Yup.object({
-   name: Yup.string().trim().required('Campo requerido').min(3, 'Debe tener al menos 3 caracteres').max(30, 'No debe tener mas de 30 caracteres'),
-   email: Yup.string().trim().required('Campo requerido').email('Correo electronico no valido').max(30, 'No debe tener mas de 30 caracteres'),
-   password: Yup.string().trim().required('Campo requerido').min(6, 'Debe tener al menos 6 caracteres').max(30, 'No debe tener mas de 30 caracteres'),
+export const registerSchema = object({
+   name: string().trim().required('Campo requerido').min(3, 'Debe tener al menos 3 caracteres').max(30, 'No debe tener mas de 30 caracteres'),
+   email: string().trim().required('Campo requerido').email('Correo electronico no valido').max(30, 'No debe tener mas de 30 caracteres'),
+   password: string().trim().required('Campo requerido').min(6, 'Debe tener al menos 6 caracteres').max(30, 'No debe tener mas de 30 caracteres'),
 })
 
-export const productSchema = Yup.object({
-   title: Yup.string().trim()
-      .required('Campo requerido'),
-   description: Yup.string().trim()
-      .required('Campo requerido'),
-   price: Yup.number()
-      .integer()
-      .typeError('Debe ser un valor numerico')
-      .min(0, 'No debe ser menor a cero'),
-   category: Yup.mixed().oneOf(ValidCategory, 'La categoria no existe'),
-   spotify: Yup.string()
-      .required('Campo requerido')
-      .trim().url('Debe ser una url valida'),
-   difficulty: Yup.number()
-      .required('Campo requerido'),
-   tags: Yup.array()
-      .of(Yup.string()).optional(),
-   images: Yup.array()
-      .of(Yup.string()).optional(),
+export const productSchema = object({
+   title: string().trim().required('Campo requerido'),
+   description: string().trim().required('Campo requerido'),
+   price: number().integer().typeError('Debe ser un valor numerico').min(0, 'No debe ser menor a cero'),
+   category: mixed().oneOf(ValidCategory, 'La categoria no existe'),
+   spotify: string().required('Campo requerido').trim().url('Debe ser una url valida'),
+   difficulty: number().required('Campo requerido'),
+   tags: array().of(string()).optional(),
+   images: array().of(string()).optional(),
 })
 
-export const paySchema = Yup.object({
-   name: Yup.string().trim()
-      .required('Campo requerido'),
-   expires: Yup.date()
-      .required('Campo requerido')
-      .typeError('Debe ser una fecha valida'),
-   number: Yup.number()
-      .required('Campo requerido')
-      .integer()
-      .typeError('Debe ser un valor numerico'),
-   csc: Yup.number()
-      .required('Campo requerido')
-      .integer()
-      .typeError('Debe ser un valor numerico')
+export const paySchema = object({
+   name: string().trim().required('Campo requerido'),
+   expires: date().required('Campo requerido').typeError('Debe ser una fecha valida'),
+   number: number().required('Campo requerido').integer().typeError('Debe ser un valor numerico'),
+   csc: number().required('Campo requerido').integer().typeError('Debe ser un valor numerico')
 })
 
-export const userSchema = Yup.object({
-   name: Yup.string().trim().min(3, 'Debe tener al menos 3 caracteres').max(30, 'No debe tener mas de 30 caracteres'),
-   email: Yup.string().trim().email('Correo electronico no valido').max(30, 'No debe tener mas de 30 caracteres'),
-   password: Yup.string().trim().min(6, 'Debe tener al menos 6 caracteres').max(30, 'No debe tener mas de 30 caracteres'),
+export const userSchema = object({
+   name: string().trim().min(3, 'Debe tener al menos 3 caracteres').max(30, 'No debe tener mas de 30 caracteres'),
+   email: string().trim().email('Correo electronico no valido').max(30, 'No debe tener mas de 30 caracteres'),
+   password: string().trim().min(6, 'Debe tener al menos 6 caracteres').max(30, 'No debe tener mas de 30 caracteres'),
 })
